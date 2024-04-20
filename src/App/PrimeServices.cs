106863f5c900af891d0722c4933657525b7e369d
sixfortyfour <sixfortyfour@users.noreply.github.com>
2024-04-20 13:41:49 +0100

    Added App and Tests

---
+++ src/App/PrimeServices.cs
@@ -0,0 +1,10 @@
+namespace PrimeServices
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    } 
+}
